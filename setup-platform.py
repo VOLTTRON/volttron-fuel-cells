34569dc79a8437aa1efb2019b15701142f7f38b6
Craig <craig.allwardt@pnnl.gov>
2018-05-15 07:38:59 -0700

    Added config store support in the platform startup.

--- setup-platform.py
+++ setup-platform.py
@@ -6,11 +6,17 @@ from time import sleep
 
 sys.stdout.write(str(os.environ))
 
-VOLTTRON_HOME = os.environ.get('VOLTTRON_HOME', "/home/volttron/.volttron")
+# The environment variables must be set or we ahve big issues
+VOLTTRON_ROOT = os.environ['VOLTTRON_ROOT']
+VOLTTRON_HOME = os.environ['VOLTTRON_HOME']
+VOLTTRON_PATH = "{}/env/bin/volttron".format(VOLTTRON_ROOT)
+VOLTTRON_CTL = "{}/env/bin/volttron-ctl".format(VOLTTRON_ROOT)
+INSTALL_PATH = "{}/scripts/install-agent.py".format(VOLTTRON_ROOT)
 KEYSTORES = os.path.join(VOLTTRON_HOME, "keystores")
 
-with open("agents.yml") as cin:
-    agents = yaml.safe_load(cin)
+with open("platform_config.yml") as cin:
+    config = yaml.safe_load(cin)
+    agents = config['agents']
 
 need_to_install = {}
 for identity, specs in agents.items():
@@ -20,7 +26,8 @@ for identity, specs in agents.items():
 
 # if we need to do installs then we haven't setup this at all.
 if need_to_install:
-    proc = subprocess.Popen(["volttron", "-vv"])
+    assert os.path.exists(VOLTTRON_PATH)
+    proc = subprocess.Popen([VOLTTRON_PATH, "-vv"])
     assert proc is not None
     sleep(5)
     with open(os.path.join(VOLTTRON_HOME, "config"), "w") as fout:
@@ -29,17 +36,41 @@ if need_to_install:
 
     config_dir = os.path.join("configs")
     for identity, spec in need_to_install.items():
-        agent_cfg = os.path.join(config_dir, spec["config"])
+        agent_cfg = None
+        if "config" in spec:
+            agent_cfg = os.path.join(config_dir, spec["config"])
 
-        assert os.path.exists(agent_cfg)
-        install_cmd = ["python", "scripts/install-agent.py"]
-        install_cmd.extend(["--agent-source", spec["source"]])
+        agent_source = os.path.expandvars(os.path.expanduser(spec['source']))
+
+        install_cmd = ["python", INSTALL_PATH]
+        install_cmd.extend(["--agent-source", agent_source])
         install_cmd.extend(["--vip-identity", identity])
         install_cmd.extend(["--start", "--priority", "50"])
         install_cmd.extend(["--agent-start-time", "2"])
-        install_cmd.extend(["--config", agent_cfg])
+        if agent_cfg:
+            install_cmd.extend(["--config", agent_cfg])
 
         subprocess.check_call(install_cmd)
+
+        if "config_store" in spec:
+            for key, entry in spec['config_store'].items():
+                entry_file = os.path.expandvars(os.path.expanduser(entry['file']))
+                entry_cmd = [VOLTTRON_CTL, "config", "store", identity, key, entry_file]
+                if "type" in entry:
+                    entry_cmd.append(entry['type'])
+
+                subprocess.check_call(entry_cmd)
+
+# _cmd(['volttron-ctl', 'config', 'store', PLATFORM_DRIVER,
+#               'fake.csv', 'examples/configurations/drivers/fake.csv', '--csv'])
+#         _cmd(['volttron-ctl', 'config', 'store', PLATFORM_DRIVER,
+#               'devices/fake-campus/fake-building/fake-device',
+#               'examples/configurations/drivers/fake.config'])
+
+
+
+
+
 
     subprocess.call(["vctl", "shutdown", "--platform"])
 
